6d816b2a3d0c2654fe38efdbf2088c400061f8ea
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-02-23 15:43:46 +0500

    models created

--- comments/models.py
+++ comments/models.py
@@ -4,7 +4,10 @@ from django.db import models
 class Comment(models.Model):
     post = models.ForeignKey('posts.Post', on_delete=models.CASCADE, related_name='comments')
     author_name = models.CharField(max_length=100)
-    author_email = models.EmailField(unique=True)
+    author_email = models.EmailField()
     content = models.TextField()
     created_at = models.DateField(auto_now_add=True)
-#    parent_comment = ?
+    parent_comment = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='replies')
+
+    def __str__(self):
+        return f"Comment from {self.author_name}"
