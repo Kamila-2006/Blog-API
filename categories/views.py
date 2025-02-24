1c37198d4e178c48bbe3edbf52f1d3b847ef2455
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-02-24 22:30:54 +0500

    categories endpoints done

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Category
+from posts.models import Post
+from django.shortcuts import get_object_or_404
+from .serializers import CategorySerializer
+from posts.serializers import PostSerializer
 
-# Create your views here.
+
+class CategoryAPIView(APIView):
+    def get(self, request):
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class CategoryPostsAPIView(APIView):
+    def get(self, request, category_id):
+        category = get_object_or_404(Category, id=category_id)
+        posts = Post.objects.filter(category=category)
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('posts.urls', namespace='posts')),
+    path('api/posts/',include('posts.urls', namespace='posts')),
+    path('api/categories/', include('categories.urls', namespace='categories')),
 ]
