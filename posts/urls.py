35159e14451226bb1a5d5b4a6cb13b928e75c750
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-02-24 20:44:08 +0500

    post endpoints done

---
+++ posts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import PostAPIView, PostDetailAPIView
+
+
+app_name = 'posts'
+
+urlpatterns = [
+    path('posts/', PostAPIView.as_view(), name='posts-list'),
+    path('posts/<int:pk>/', PostDetailAPIView.as_view(), name='post-detail'),
+]
