c8e3b8ada94417e592522c4bb577252a3a19cbaa
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-02-24 22:01:17 +0500

    comments endpoints done

--- posts/urls.py
+++ posts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import PostAPIView, PostDetailAPIView
+from comments.views import CommentAPIView
 
 
 app_name = 'posts'
@@ -7,4 +8,5 @@ app_name = 'posts'
 urlpatterns = [
     path('posts/', PostAPIView.as_view(), name='posts-list'),
     path('posts/<int:pk>/', PostDetailAPIView.as_view(), name='post-detail'),
+    path('posts/<int:post_id>/comments/', CommentAPIView.as_view(), name='post-comments')
 ]
