148353d252f5f56a740a05c788fd329f25260286
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-02-24 22:59:32 +0500

    endpoints done

--- posts/urls.py
+++ posts/urls.py
@@ -6,7 +6,7 @@ from comments.views import CommentAPIView
 app_name = 'posts'
 
 urlpatterns = [
-    path('posts/', PostAPIView.as_view(), name='posts-list'),
+    path('', PostAPIView.as_view(), name='posts-list'),
     path('posts/<int:pk>/', PostDetailAPIView.as_view(), name='post-detail'),
     path('posts/<int:post_id>/comments/', CommentAPIView.as_view(), name='post-comments')
 ]
