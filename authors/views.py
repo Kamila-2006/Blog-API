0d3209d845519d76f1886893109d7e1292f2b5f7
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-03-06 14:15:15 +0500

    completed

--- authors/views.py
+++ authors/views.py
@@ -10,10 +10,3 @@ class AuthorAPIView(APIView):
         authors = Author.objects.all()
         serializer = AuthorSerializer(authors, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def post(self, request):
-        serializer = AuthorSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- categories/views.py
+++ categories/views.py
@@ -14,13 +14,6 @@ class CategoryAPIView(APIView):
         serializer = CategorySerializer(categories, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def post(self, request):
-        serializer = CategorySerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 class CategoryPostsAPIView(APIView):
     def get(self, request, category_id):
         category = get_object_or_404(Category, id=category_id)
